ee919b223da997d5457f16f81a01c438eb531466
GauthamCraezy <craezyengineer@gmail.com>
2015-02-18 19:51:50 +0530

    java script samples

---
+++ scripts/Game/search.js
@@ -0,0 +1,21 @@
+var text = "Declare Gautham and set the Gautham following three variables Gautham:";
+var myName = "Gautham";
+var hits = [];
+for(var i=0; i<text.length; i++)
+{
+    if(text[i] === myName[0])
+    {
+        for(var j=i; j<myName.length+i; j++)
+        {
+            hits.push(text[j]);
+            }
+        }
+}
+if(hits.length<=0)
+{
+    console.log("Your name wasn't found!");
+    }
+    else
+    {
+        console.log(hits);
+        }
